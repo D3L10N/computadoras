fc06593518e6830f7eb31098310d1240df894ddd
D3L10N <115430876+D3L10N@users.noreply.github.com>
2022-10-13 22:41:16 -0500

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,6 @@
+function cambiarClase(){
+    var siteNave = document.getElementById('menu');
+        siteNave.classList.toggle('site-nav-open');
+    var menuOpen = document.getElementById('menu-toggle');
+        siteNave.classList.toggle('menu-open');
+}
